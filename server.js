effec998115fe7834c7b411d33825dc35de172a6
Student <student@PNCLB32-08.pnc.lan>
2021-05-22 23:32:44 +0700

    index@@d

---
+++ server.js
@@ -0,0 +1,27 @@
+// const express = require('express');
+// const app = express();
+
+// app.listen(process.env.PORT || 5000, () => console.log("Server running..."));
+
+// app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(express.static('public'));
+
+// app.get('/', (req, res) => res.send("Hello Project"));
+
+
+function longinDisplay(){
+    form.style.display= "block";
+    chat.style.display= "none";
+    console.log("hello")
+   
+}
+let chat = document.querySelector('.class-chat');
+
+let form = document.querySelector('.form');
+
+
+let btnf = document.getElementById('login');
+btnf.addEventListener('click' ,longinDisplay);
+
+
