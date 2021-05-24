15da1a06754374fcf3250dc47cf8d260b016f225
Student <student@PNCLB32-08.pnc.lan>
2021-05-24 10:45:15 +0700

    a@dindex

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,7 @@
+const IP = "192.168.1.9";
+const PORT = 3000;
+const GET_MESSAGE_REQUEST = "http://" + IP + ":" + PORT + "/message";
+
 function addfood(event) {
     let chat_box = document.getElementById('chat_box');
     let van = document.createElement('div');
@@ -15,3 +19,27 @@ const text = document.getElementById('text');
 const addForm = document.querySelector("#send");
 addForm.addEventListener("click", addfood);
 //.........hide show.......
+
+function longinDisplay() {
+    form.style.display = "block";
+    chat.style.display = "none";
+    row.style.display = 'none';
+    console.log("hello")
+
+}
+function displayMwssage() {
+    event.preventDefault();
+    form.style.display = "none";
+    chat.style.display = "none";
+    row.style.display = 'block';
+}
+let row = document.querySelector('.row');
+let log = document.getElementById('log');
+log.addEventListener('click', displayMwssage);
+let chat = document.querySelector('.class-chat');
+
+let form = document.querySelector('.form');
+
+
+let btnf = document.getElementById('login');
+btnf.addEventListener('click', longinDisplay);
--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// app.listen(process.env.PORT || 5000, () => console.log("Server running..."));
+app.listen(process.env.PORT || 5000, () => console.log("Server running..."));
 
-// app.use(express.json());
-// app.use(express.urlencoded());
-// app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded());
 
-// app.get('/', (req, res) => res.send("Hello Project"));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send("Hello Project"))
+
+let users = [
+    {username: "sreypich", color: "red", text: "hello everyone how are you"},
+    {username: "chanthea", color: "green", text: "yes hello i'm fine. thank"},
+
+]
+
+// show data on browser
+app.get('/users', (req , res) =>{
+    res.send(users);
+})
 
 
-function longinDisplay() {
-    form.style.display = "block";
-    chat.style.display = "none";
-    console.log("hello")
-
-}
-let chat = document.querySelector('.class-chat');
-
-let form = document.querySelector('.form');
+// get users to updat
+app.post('/users', (req, res) =>{
+    
+    let user = {text:req.body.text};
+    users.push(user);
+    res.send(users);
 
 
-let btnf = document.getElementById('login');
-btnf.addEventListener('click', longinDisplay);
+})
+
