d0bc28fd8ddf6f1acf24008322c86a508e6c362f
Student <student@PNCLB32-10.pnc.lan>
2021-05-25 08:22:27 +0700

    add index

--- server.js
+++ server.js
@@ -1,34 +1,24 @@
-const express = require('express');
+
+
+const express = require('express')
 const app = express();
 
-app.listen(process.env.PORT || 5000, () => console.log("Server running..."));
-
+app.listen(process.env.PORT || 5000);
 app.use(express.json());
 app.use(express.urlencoded());
-
 app.use(express.static('public'));
-
-app.get('/', (req, res) => res.send("Hello Project"))
 
 let users = [
     {username: "sreypich", color: "red", text: "hello everyone how are you"},
     {username: "chanthea", color: "green", text: "yes hello i'm fine. thank"},
 
 ]
+app.get ('/users',(req,res)=>res.send(users))
 
-// show data on browser
-app.get('/users', (req , res) =>{
-    res.send(users);
-})
 
 
 // get users to updat
-app.post('/users', (req, res) =>{
-    
-    let user = {text:req.body.text};
-    users.push(user);
-    res.send(users);
 
 
-})
+
 
