d86e366b9aabf22329fbdef76756139cb9b84e7c
Student <student@PNCLB32-10.pnc.lan>
2021-05-29 18:15:57 +0700

    add index

--- public/js/app.js
+++ public/js/app.js
@@ -50,7 +50,7 @@ btnBack.addEventListener('click', backLogin);
 let messa = document.querySelector("#text");
 let btnSend = document.querySelector("#send");
 btnSend.addEventListener("click", sendMesage);
-let url = "http://localhost:5000";
+let url = "https://f-chat-app-project.herokuapp.com/";
 displayMessage();
 
 
